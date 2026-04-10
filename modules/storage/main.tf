a05acb7018dd7a324337325482a99519db117ddc
ShenLoong99 <yithuettsk29@gmail.com>
2026-04-10 18:17:42 +0800

    fix checkov errors

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -28,11 +28,45 @@ resource "aws_s3_bucket_lifecycle_configuration" "receipt_lifecycle" {
       days = 1
     }
 
-    # Clean up unfinished uploads to save space
+    # Abort failed uploads after 7 days to save money
     abort_incomplete_multipart_upload {
-      days_after_initiation = 1
+      days_after_initiation = 7
+    }
+
+    # If you enabled versioning, delete non-current versions after 7 days
+    noncurrent_version_expiration {
+      noncurrent_days = 7
     }
   }
+}
+
+# Enable versioning on the S3 bucket
+resource "aws_s3_bucket_versioning" "versioning_receipt_storage" {
+  bucket = aws_s3_bucket.receipt_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.receipt_storage.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block all public access to the bucket
+resource "aws_s3_bucket_public_access_block" "receipt_storage_access" {
+  bucket = aws_s3_bucket.receipt_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # S3 Trigger Permission
